c44f9e4c4044bf71b7546a440617d7aefd8f2ae3
Bugrova Irina <bugrova147@yandex.ru>
2024-11-10 18:20:34 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -2,8 +2,14 @@ def filter_by_state(list_of_dicts: list, state="EXECUTED") -> list:
     """Функция, которая фильтрует список по статусу"""
     new_list_of_dicts = []
 
-    for dict in list_of_dicts:
-        for value in dict.values():
+    for dictionary in list_of_dicts:
+        for value in dictionary.values():
             if value == state:
-                new_list_of_dicts.append(dict)
+                new_list_of_dicts.append(dictionary)
     return new_list_of_dicts
+
+
+def sort_by_date(list_of_dicts: list, reverse: bool = True) -> list:
+    """Функция, которая сортирует список по дате"""
+    sorted_list_of_dicts = sorted(list_of_dicts, key=lambda dictionary: dictionary["date"], reverse=reverse)
+    return sorted_list_of_dicts
