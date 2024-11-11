bb45fc73260e2ef48e14582caab121732ed1c8f1
Bugrova Irina <bugrova147@yandex.ru>
2024-11-11 21:47:28 +0300

    homework 10.1 done

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
-    """Функция, которая фильтрует список по статусу"""
+    """Функция, фильтрующая список по статусу"""
     new_list_of_dicts = []
 
     for dictionary in list_of_dicts:
@@ -10,6 +10,6 @@ def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
 
 
 def sort_by_date(list_of_dicts: list, reverse: bool = True) -> list:
-    """Функция, которая сортирует список по дате"""
+    """Функция, сортирующая список по дате"""
     sorted_list_of_dicts = sorted(list_of_dicts, key=lambda dictionary: dictionary["date"], reverse=reverse)
     return sorted_list_of_dicts
