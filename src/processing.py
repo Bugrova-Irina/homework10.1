fdfbb73deb8c626b745a108fddc03e6e7fb68be5
Bugrova Irina <bugrova147@yandex.ru>
2024-11-12 11:42:30 +0300

    correction of errors, detected by the flake8

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(list_of_dicts: list[dict[str, any]], state: str = "EXECUTED") -> list[dict[str, any]]:
+from typing import Any
+
+
+def filter_by_state(list_of_dicts: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция, фильтрующая список по статусу"""
     new_list_of_dicts = []
 
@@ -9,7 +12,7 @@ def filter_by_state(list_of_dicts: list[dict[str, any]], state: str = "EXECUTED"
     return new_list_of_dicts
 
 
-def sort_by_date(list_of_dicts: list[dict[str, any]], reverse: bool = True) -> list[dict[str, any]]:
+def sort_by_date(list_of_dicts: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
     """Функция, сортирующая список по дате"""
     sorted_list_of_dicts = sorted(list_of_dicts, key=lambda dictionary: dictionary["date"], reverse=reverse)
     return sorted_list_of_dicts
